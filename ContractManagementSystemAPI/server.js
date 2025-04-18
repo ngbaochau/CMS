d4f4a3702e768e2ecc299796125a36a0cf1b7797
BuildTools <unconfigured@null.spigotmc.org>
2025-04-18 13:54:48 +0700

    update

--- ContractManagementSystemAPI/server.js
+++ ContractManagementSystemAPI/server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-
+import express from "express";
+import dotenv  from "dotenv"
+dotenv.config();
 const app = express();
 app.use(express.json());
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 
 
