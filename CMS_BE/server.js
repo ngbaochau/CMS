04f1c484039bee99fce58e4edc736ab51a423042
DangPhuc <dangphucvghy195@gmail.com>
2025-04-23 18:18:29 +0700

    fix:update file backend

--- CMS_BE/server.js
+++ CMS_BE/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './src/config/db.js';
+import projectRoutes from './src/routers/projectRoutes.js';
 import cors from 'cors';
 
 dotenv.config();
@@ -11,14 +12,14 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  }),
+  })
 );
 
 app.get('/', (req, res) => {
   res.status(200).send('Server running on ' + process.env.BACKEND_URL);
 });
-
+app.use('/projects', projectRoutes);
 try {
   await sequelize.authenticate();
-  app.listen(process.env.PORT || 4000);
+  app.listen(process.env.PORT || 5000);
 } catch {}
